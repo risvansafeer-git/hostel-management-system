052a5dec7f83afb918c2d8e443924710ad8f0ed5
risvansafeer-git <risvansafeer@gmail.com>
2026-06-15 20:52:49 +0530

    Add room management module

--- server.js
+++ server.js
@@ -38,3 +38,37 @@ app.get("/students", (req, res) => {
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
+app.post("/rooms", (req, res) => {
+
+    const { room_number, capacity } = req.body;
+
+    const sql =
+        "INSERT INTO rooms(room_number, capacity) VALUES (?, ?)";
+
+    db.query(sql, [room_number, capacity], (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json({
+            message: "Room added successfully"
+        });
+
+    });
+
+});
+app.get("/rooms", (req, res) => {
+
+    db.query("SELECT * FROM rooms", (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json(result);
+
+    });
+
+});
