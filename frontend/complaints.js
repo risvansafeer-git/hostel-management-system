4f4c2472d0710e3139a350d8f6d0274815800e45
risvansafeer-git <risvansafeer@gmail.com>
2026-06-17 20:40:46 +0530

    Add complaint management frontend

---
+++ frontend/complaints.js
@@ -0,0 +1,101 @@
+async function loadStudents() {
+
+    const response =
+        await fetch(
+            "http://localhost:3000/students"
+        );
+
+    const students =
+        await response.json();
+
+    const select =
+        document.getElementById(
+            "studentSelect"
+        );
+
+    select.innerHTML = "";
+
+    students.forEach(student => {
+
+        select.innerHTML += `
+            <option value="${student.id}">
+                ${student.name}
+            </option>
+        `;
+
+    });
+
+}
+
+async function submitComplaint() {
+
+    const complaint = {
+
+        student_id:
+            document.getElementById(
+                "studentSelect"
+            ).value,
+
+        description:
+            document.getElementById(
+                "description"
+            ).value
+
+    };
+
+    await fetch(
+        "http://localhost:3000/complaints",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+            body:
+                JSON.stringify(
+                    complaint
+                )
+        }
+    );
+
+    loadComplaints();
+
+}
+
+async function loadComplaints() {
+
+    const response =
+        await fetch(
+            "http://localhost:3000/complaints"
+        );
+
+    const complaints =
+        await response.json();
+
+    const list =
+        document.getElementById(
+            "complaintList"
+        );
+
+    list.innerHTML = "";
+
+    complaints.forEach(complaint => {
+
+        list.innerHTML += `
+            <li>
+                Complaint ID:
+                ${complaint.id}
+                |
+                Status:
+                ${complaint.status}
+                <br>
+                ${complaint.description}
+            </li>
+        `;
+
+    });
+
+}
+
+loadStudents();
+loadComplaints();
