73fa552a579631f75dfe52c513d77fbb1a1787d5
risvansafeer-git <risvansafeer@gmail.com>
2026-06-19 08:05:06 +0530

    added navigation bar

--- frontend/rooms.js
+++ frontend/rooms.js
@@ -46,20 +46,114 @@ async function loadRooms() {
 
     roomList.innerHTML = "";
 
-    rooms.forEach(room => {
+   rooms.forEach(room => {
 
-        roomList.innerHTML += `
-            <li>
-                Room:
-                ${room.room_number}
-                |
-                Capacity:
-                ${room.capacity}
-            </li>
-        `;
+    roomList.innerHTML += `
+        <li>
 
-    });
+            Room:
+            ${room.room_number}
+
+            |
+
+            Capacity:
+            ${room.capacity}
+
+            <button
+                onclick="
+                editRoom(
+                    ${room.id},
+                    '${room.room_number}',
+                    '${room.capacity}'
+                )">
+
+                Edit
+
+            </button>
+
+            <button
+                onclick="
+                deleteRoom(
+                    ${room.id}
+                )">
+
+                Delete
+
+            </button>
+
+        </li>
+    `;
+
+});
 
 }
 
 loadRooms();
+function editRoom(
+    id,
+    room_number,
+    capacity
+) {
+
+    document.getElementById(
+        "roomId"
+    ).value = id;
+
+    document.getElementById(
+        "room_number"
+    ).value = room_number;
+
+    document.getElementById(
+        "capacity"
+    ).value = capacity;
+
+}
+async function updateRoom() {
+
+    const id =
+        document.getElementById(
+            "roomId"
+        ).value;
+
+    const room = {
+
+        room_number:
+            document.getElementById(
+                "room_number"
+            ).value,
+
+        capacity:
+            document.getElementById(
+                "capacity"
+            ).value
+
+    };
+
+    await fetch(
+        `http://localhost:3000/rooms/${id}`,
+        {
+            method: "PUT",
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+            body:
+                JSON.stringify(room)
+        }
+    );
+
+    loadRooms();
+
+}
+async function deleteRoom(id) {
+
+    await fetch(
+        `http://localhost:3000/rooms/${id}`,
+        {
+            method: "DELETE"
+        }
+    );
+
+    loadRooms();
+
+}
